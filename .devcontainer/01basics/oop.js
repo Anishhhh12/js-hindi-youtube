2d33c027d18ed7ffd1ab8150cdf60dd73bf0f7c3
Anishhhh12 <165990614+Anishhhh12@users.noreply.github.com>
2024-07-23 17:06:09 +0000

    command

---
+++ .devcontainer/01basics/oop.js
@@ -0,0 +1,31 @@
+const user = {
+    username : "hitesh",
+    loginCount :8,
+    signedIn : true,
+    
+    getUserDetails: function(){
+        // console.log("got user details from userbase");
+        // console.log(`Username: ${username}`);
+        console.log(this);
+    }
+}
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+function User(username,loginCount,isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this
+}
+
+const userOne = new User("anish",12,true);
+const userTwo = new User("hitesh",11,true);
+console.log(userOne);
+console.log(userTwo);
+// new gives a new object donot overWrite.
+// data pass on
+// constructor is refrence of ourself.
+// instanceof read from mdn
